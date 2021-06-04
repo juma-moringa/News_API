88124585f94e5b68de5f9ea5019963bea7012363
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-04 22:55:46 +0300

    updated the apps confirguration

--- config.py
+++ config.py
@@ -1,34 +1,37 @@
 import os
 
+
 class Config:
     '''
     General configuration parent class
     '''
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
-    
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
+        '''
+        Production  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+        Args:
+            Config: The parent configuration class with General configuration settings
+        '''
+        pass
 
 
 class DevConfig(Config):
-    '''
-    Development  configuration child class
+        '''
+        Development  configuration child class
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
+        Args:
+            Config: The parent configuration class with General configuration settings
+        '''
+        DEBUG = True
 
-    DEBUG = True
 
 config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
-    }
-    
+    'development': DevConfig,
+    'production': ProdConfig
+}
