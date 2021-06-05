048143fa08d5192e115024698dd6139cc2a20b43
juma-moringa <allan.juma@student.moringaschool.com>
2021-06-05 16:07:39 +0300

    arranged the cards display

--- config.py
+++ config.py
@@ -1,37 +1,64 @@
+# import os
+
+
+# class Config:
+#     '''
+#     General configuration parent class
+#     '''
+#     ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+#     NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+#     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+#     SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+# class ProdConfig(Config):
+#         '''
+#         Production  configuration child class
+
+#         Args:
+#             Config: The parent configuration class with General configuration settings
+#         '''
+#         pass
+
+
+# class DevConfig(Config):
+#         '''
+#         Development  configuration child class
+
+#         Args:
+#             Config: The parent configuration class with General configuration settings
+#         '''
+#         DEBUG = True
+
+
+# config_options = {
+#     'development': DevConfig,
+#     'production': ProdConfig
+# }
+
+
+
+
 import os
 
-
 class Config:
-    '''
-    General configuration parent class
-    '''
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
-    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    NEWS_API_KEY = os.environ.get('MOVIE_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+   	NEWS_SOURCES_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+   	ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+   	NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+   
+   	pass
 
 
 class ProdConfig(Config):
-        '''
-        Production  configuration child class
-
-        Args:
-            Config: The parent configuration class with General configuration settings
-        '''
-        pass
+    pass
 
 
 class DevConfig(Config):
-        '''
-        Development  configuration child class
-
-        Args:
-            Config: The parent configuration class with General configuration settings
-        '''
-        DEBUG = True
-
+    DEBUG = True
 
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig
+'development':DevConfig,
+'production':ProdConfig
+
 }
--- manage.py
+++ manage.py
@@ -1,14 +1,13 @@
 from app import create_app
 from flask_script import Manager,Server
 
-
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
-@manager.command
 
+@manager.command
 def test():
     """Run the unit tests."""
     import unittest
